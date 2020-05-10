2473d98db5976809ceb3a8a344070c3b7168a852
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-05-09 22:29:14 -0400

    Use readline module for better prompt with message composer utility

--- controller/message_composer.py
+++ controller/message_composer.py
@@ -1,5 +1,18 @@
 import controller
 import json
+import readline
+import os
+import atexit
+
+histfile = os.path.join(os.path.expanduser("~"), ".vibrance_history")
+try:
+    readline.read_history_file(histfile)
+    # default history len is -1 (infinite), which may grow unruly
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    pass
+
+atexit.register(readline.write_history_file, histfile)
 
 ctrl = controller.Controller("cloud.itsw.es")
 
