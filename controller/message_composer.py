f3deb763f61d82a1037ceb573e826157c9b4be88
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-05-09 22:38:03 -0400

    Accept relay server as command line argument for message composer util

--- controller/message_composer.py
+++ controller/message_composer.py
@@ -2,21 +2,26 @@ import controller
 import json
 import readline
 import os
+import sys
 import atexit
 
 histfile = os.path.join(os.path.expanduser("~"), ".vibrance_history")
+
 try:
     readline.read_history_file(histfile)
-    # default history len is -1 (infinite), which may grow unruly
     readline.set_history_length(1000)
 except FileNotFoundError:
     pass
 
 atexit.register(readline.write_history_file, histfile)
 
-ctrl = controller.Controller("cloud.itsw.es")
+if len(sys.argv) < 2:
+    print("Usage: message_composer.py [relay address]")
+    sys.exit()
+
+ctrl = controller.Controller(sys.argv[1])
 
 while True:
     i = input("Messages> ")
     ctrl.messages = json.loads(i)
-    ctrl.write()
+    print(ctrl.write())
